1afd12fbd2c587c265acc4d6766b9866b59220ab
ALIASHRAF777 <welamehar777@gmail.com>
2022-09-14 14:47:18 +0500

    MongoDB Atlas connection

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -5,9 +5,7 @@ import User from "../models/userModels.js";
 
 const seedRouter = express.Router();
 seedRouter.get("/", async (req, res) => {
-  await Product.remove({});
   const CreateProducts = await Product.insertMany(data.products);
-  await User.remove({});
   const createdUsers = await User.insertMany(data.users);
   res.send({ CreateProducts, createdUsers });
 });
